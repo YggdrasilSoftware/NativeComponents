0afeef4427b58ba54a7b3a2f6556e065154e56dd
Salih Burgul <salihburgul98@gmail.com>
2023-03-21 00:06:14 +0300

    ios-NativeModules

---
+++ ios/CustomView.m
@@ -0,0 +1,20 @@
+//
+//  CustomView.m
+//  NativeComponents
+//
+//  Created by Necdet Salih Burgul on 20.03.2023.
+//
+
+#import "CustomView.h"
+
+@implementation CustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ ios/NativeComponents/CustomView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomView.swift
+//  NativeComponents
+//
+//  Created by Necdet Salih Burgul on 20.03.2023.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
